83a2866e3d36523bfe7a54325358f3d8a54e676e
Sarah Tirey <sarahtirey@gmail.com>
2021-03-14 10:37:37 -0400

    added notes to node.js

--- node.js
+++ node.js
@@ -29,3 +29,26 @@ for (let i = 0; i < 1000; i++){
 }
 
 console.log(`Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${process.memoryUsage().heapUsed}. \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
+
+// FILE SYSTEM NOTES //
+// Node fs core module is an API for interacting with the file system
+// Has both synchronous & asynchronous version
+// .readFile method reads data from a provided file
+//EXAMPLE
+
+// required the fs core module
+const fs = require('fs');
+
+// define an error first callback function
+let readDataCallback = (err, data) => {
+    if (err) {
+        console.log(`Something went wrong: ${err}`);
+    } else {
+        console.log(`Provided file contained: ${data}`);
+    }
+    }
+// invoked the readFile() method with three arguments
+// a string that contains a path to the file
+// a string specifying the files character encoding, utf-8 for text files
+// callback function to be invoked when the asychronous task of reading from the file system is complete.Node will pass the contents of file.txt into the provided callback as its second argument
+    fs.readFile('./file.txt', 'utf-8', readDataCallback)
